143ae51a2a332146b4aa53f38bc9236d739175ac
ForgeableSum <chrisscott11us@yahoo.com>
2026-07-24 04:05:23 -0400

    Revert "Block user input between lobby actions"
    This reverts commit 0a3e46d07ac25698fb1eefe70ed32b22504f82d6.

--- src/main/aoe2Win32Automation.ts
+++ src/main/aoe2Win32Automation.ts
@@ -204,6 +204,8 @@ export async function clickAoe2DesignPoint(
     restored = Boolean(SetCursorPos!(original.x, original.y));
   }
 
+  await delay(500);
+
   return {
     sent: true,
     detail: [
