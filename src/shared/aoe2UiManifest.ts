e34304a1e41c00cb4495f971e2838d0fa6e8b19d
ForgeableSum <chrisscott11us@yahoo.com>
2026-08-14 08:16:49 -0400

    Shorten lobby automation waits

--- src/shared/aoe2UiManifest.ts
+++ src/shared/aoe2UiManifest.ts
@@ -52,7 +52,7 @@ export const aoe2UiManifest = {
       label: "Create Lobby",
       point: [1688, 1614],
       activation: "click",
-      settleMs: 8_000
+      settleMs: 4_000
     },
     hostInvite: {
       label: "Invite",
